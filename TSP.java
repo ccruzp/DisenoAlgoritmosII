32ccb78c41637e6c485d675e5aa349431a28ef32
daralion <gabrielgedler@gmail.com>
2015-05-04 09:02:25 -0430

    no message

--- TSP.java
+++ TSP.java
@@ -1,87 +1,113 @@
+/*
+NOTA: estoy leyendo directamente de el archivo porque fuck da police
+LAS ESTRUCTURAS SE USARON PORQUE    
+numNodos: Permite la iteracion a traves de los elementos sencillamente,
+          sintener que recurrir a funciones de tamaño de arreglos
+caminos:  Asigna sencillamente a cada elemento su siguiente. Permite conocer
+          el sucesor en orden lineal, lo cual ayuda enormemente para optimizacion 2-opt.
+          Tambien otorga la idea de un ciclo dirigido, que tiene mas sentido 
+          que con una matriz de adyacencias.
+costos:   Permite realizar busquedas de costos en orden constante. Como los costos
+          son preprocesados, el costo(redundancia) se amortiza sobre el tiempo
+recorrido:bleh
+
+ */
+package tsp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 /*
   Clase: TSP
-  Función: implementa el TSP. Esta es la clase principal.
-  Parámetros:
-  - numNodos: representa el número de nodos que tiene el grafo.
+  FunciÃ³n: implementa el TSP. Esta es la clase principal.
+  ParÃ¡metros:
+  - numNodos: representa el nÃºmero de nodos que tiene el grafo.
   - listaNodos: es un arreglo que almacena todos los nodos del grafo.
-  - caminos: es una matriz de int para representar el camino óptimo.
-             Se guarda 1 en la posición (i, j) si existe un subcamino
-             que pase por los nodos i y j; si no, se guarda 0.
-  - costos: Es una matriz de double que guarda en cada posición (i, j) 
+  - caminos: Arreglo que posee para cada elemento, a cual se mueve (la siguiente ciudad a recorrer)
+
+  - costos: Es una matriz de double que guarda en cada posiciÃ³n (i, j) 
             el costo de viajar entre el nodo i y el nodo j, y viceversa.
+  - vecinos: Lista ordenada de los nodos mas cercanos a cada nodo.
+  - recorrido: define el costo total de hacer un recorrido.
 */
 public class TSP {
     static int numNodos;
-    static int[][] caminos;
+    static int[] caminos;
     static double[][] costos;
-    //static Nodo[] listaNodos;
+    static double recorrido;
+
 
     public static void main(String args[]) {
-	// listaNodos = Reader.readInput();
 	TSP tsp = new TSP(Reader.readInput());
-	for(int i = 0; i < numNodos; ++i) {
-	    for(int j = 0; j < numNodos; ++j) {
-		System.out.print(costos[i][j] + " ");
-	    }
-	    System.out.println();
+        
+        //Crea un camino inicial mediante busqueda de vecinos (Nearest Neighbor)
+        int[] set = new int[numNodos];
+        Arrays.fill(set,0);
+        int start = 30;          //Nodo donde comienza y cierra el ciclo
+        int num = numNodos;
+        int l = start;
+	while (num-- > 1) {
+            double menor = Double.MAX_VALUE;
+            int nodoDes = 0;
+            for(int j = 0; j < numNodos; ++j) {
+                if (costos[l][j] <= menor && set[j] < 1 && (l != j)) {
+                    menor = costos[l][j];
+                    nodoDes = j;
+                }
+            }
+            caminos[l] = nodoDes;
+            recorrido += menor;
+            set[l]++;
+            set[nodoDes]++;
+            l = nodoDes;
+            
 	}
+        caminos[l] = start;
+        recorrido += costos[l][start];
+        System.out.println("METODO DEL VECINO MAS CERCANO");
+        System.out.println("Costo total= " + recorrido);
+        
+        //Optimiza el camino ya creado mediante 2-opt
+        boolean improvement = true;
+        while(improvement) {
+            improvement = false;
+            for (int i = 0; i < numNodos-1; i++) {
+                for (int j = i+1; j < numNodos; j++) {
+                    //busca los candidatos
+                    int i2 = caminos[i];
+                    int j2 = caminos[j];
+                        double costoActual = costos[i][i2] + costos[j][j2];
+                        double nuevoCosto = costos[i][j] + costos[i2][j2];
+                        if (costoActual > nuevoCosto) {
+                            improvement = true;
+                            recorrido = recorrido - costoActual + nuevoCosto;
+                            caminos[i] = j;
+                            caminos[i2] = j2;
+                        }
+                }
+            }
+        }    
+        System.out.println("OPTIMIZACION 2-OPT");
+        System.out.println("Costo total= " + recorrido);
     }
 
+    
     public TSP (double[][] costos) {
 	this.costos = costos;
 	this.numNodos = costos.length;
-	caminos = new int[numNodos][numNodos];
-	// costos = new double[numNodos][numNodos];
-	for(int i = 0; i < numNodos; i++) {
-	    for(int j = 0; j < numNodos; j++) {
-		caminos[i][j] = 0;
-	    }
-	}
+        recorrido = 0;
+	caminos = new int[numNodos];
+        
+       
     }
-
-    /*
-      Método: getCosto.
-      Función: calcula el costo de ir del nodo id1 al nodo id2.
-    */
-    // public double getCosto(int id1, int id2) {
-	
-    // 	if (costos[id1-1][id2-1] == -1) {
-    // 	    costos[id1-1][id2-1] = Math.sqrt(Math.pow(listaNodos[id2-1].x - listaNodos[id1-1].x, 2) + Math.pow(listaNodos[id2-1].y - listaNodos[id1-1].y, 2));
-    // 	    costos[id2-1][id1-1] = costos[id1-1][id2-1];
-    // 	}
-    // 	return costos[id1-1][id2-1];
-    // }
 }
 
-/*
-  Clase: Nodo
-  Función: clase que implementa los nodos del grafo.
-  Parámetros:
-  - id: int que identifica al nodo.
-  - x: coordenada X del nodo en un plano.
-  - y: coordenada Y del nodo en un plano.
-*/
-// class Nodo {
-//     int id;
-//     int x;
-//     int y;
-
-//     public Nodo(int id, int x, int y) {
-// 	this.id = id;
-// 	this.x = x;
-// 	this.y = y;
-//     }
-
-//     public String toString() {
-// 	return "Nodo " + Integer.toString(id);
-//     }
-// }
 
 /*
   Clase: Reader
-  Función: procesa el archivo de entrada y crea la lista de nodos.
+  FunciÃ³n: procesa el archivo de entrada y crea la lista de nodos.
 */
 
 class Reader {
@@ -91,25 +117,32 @@ class Reader {
 
     /*
       Método: readInput.
-      Función: lee la información por entrada estándar y crea la lista de nodos.
+      FunciÃ³n: lee la información por entrada estándar 
+      (mediante uno de los archivos output.txt creados con Preprocessor)
     */
     public static double[][] readInput() {
-	Scanner scanner = new Scanner(System.in);
-	// scanner.nextLine();
-	// scanner.nextLine();
-	// scanner.nextLine();
-	// int numNodos = Integer.parseInt(scanner.nextLine().split(" ")[1]);
-	int numNodos = scanner.nextInt();
-	double[][] l = new double[numNodos][numNodos];
-	// scanner.nextLine();
-	// scanner.nextLine();
-	for(int i = 0; i < numNodos; ++i) {
-	    scanner.nextDouble();
-	    for(int j = 0; j < numNodos; ++j) {
-		// l[i] = new Nodo(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
-		l[i][j] = scanner.nextDouble();
-	    }
-	}
-	return l;
+        File file = new File("C:\\Users\\GabrielAugusto\\Desktop\\Projects\\DAII\\output_a280.txt");
+        try {
+            
+            Scanner scanner = new Scanner(file);
+            int numNodos = scanner.nextInt();
+            double[][] l = new double[numNodos][numNodos];
+
+            for(int i = 0; i < numNodos; ++i) {
+                scanner.nextDouble();
+                for(int j = 0; j < numNodos; ++j) {
+                    l[i][j] = scanner.nextDouble();
+                }
+            }
+            for (int i = 0; i < numNodos; i++) {
+                l[i][i] = 1000000;
+            }
+            return l;
+            
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not Found");
+        } 
+        return null;
     }
 }
